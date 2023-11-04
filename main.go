8346fefa859d40e4cd8ac5d5a485994b71976f91
Nick Hale <4175918+njhale@users.noreply.github.com>
2023-11-03 22:27:21 -0400

    add port path
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"sync"
 )
 
 func main() {
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		tee := io.MultiWriter(res, log.Writer())
-		fmt.Fprintln(tee, "Hello Acorn!")
+		fmt.Fprintln(tee, "Greetings!")
 	})
 
-	log.Fatal(http.ListenAndServe(":9000", nil))
+	var (
+		yaaargs int
+		mu      sync.RWMutex
+		stop    = make(chan struct{})
+	)
+	mux.HandleFunc("/ahoy", func(res http.ResponseWriter, req *http.Request) {
+		tee := io.MultiWriter(res, log.Writer())
+		fmt.Fprintln(tee, "YAAARG!")
+		mu.Lock()
+		defer mu.Unlock()
+		yaaargs++
+		if yaaargs > 3 {
+			close(stop)
+		}
+	})
+
+	srv := &http.Server{Addr: ":9000", Handler: mux}
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-stop
+		if err := srv.Shutdown(context.Background()); err != nil {
+			log.Println(err)
+		}
+		return
+	}()
+
+	log.Println(srv.ListenAndServe())
+
+	wg.Wait()
 }
